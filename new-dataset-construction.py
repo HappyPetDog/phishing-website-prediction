f838083decdd37d2cab23942b19a7c29025834af
Masayuki Inukai <mzinukai@up.edu.ph>
2024-06-05 14:07:16 +0800

    feat: remove old datasets and finish new dataset construction

--- new-dataset-construction.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-training_set = pd.read_csv('./datasets/training.csv')
-testing_set = pd.read_csv('./datasets/testing.csv')
-
-# Concatenating the dataframes into one
-final_dataset = pd.concat([training_set, testing_set]).reset_index(drop=True)
-print(final_dataset.head())
-final_dataset.to_csv('./datasets/new_dataset.csv', index=False)
